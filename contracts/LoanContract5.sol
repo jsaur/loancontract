5659d2808b5ef457b3655d8299e052cea045574f
Jacob <jsaur@kiva.org>
2017-02-02 10:24:47 -0800

    Create LoanContract5.sol

---
+++ contracts/LoanContract5.sol
@@ -0,0 +1,182 @@
+pragma solidity ^0.4.8;
+/* Introduces the concept of a share price. This prevents someone lending 1 wei and makes distribution
+   logic simplier. If a lender lends more than the share price we return to sender. Same if the borrower
+   sends more than can be evenly distributed 
+*/
+contract LoanContract5 {
+    //Constructor vars
+    address borrowerAddress;
+    uint public loanAmount;
+    uint public fundRaisingDeadline;
+    uint public repaymentDeadline;
+    
+    //Useful vars
+    uint public numLenders;
+    uint public amountRaised;
+    uint public amountRepaid;
+    uint public sharePrice = 500000000000000000; //0.5 ether
+    /* Since solidity doesn't all iterating over mapping, we need 2 data structures to represent lender accounts */
+    address[] public lenderAddresses;
+    mapping(address => LenderAccount) public lenderAccounts;
+    enum State {raising, funded, repaying, repaid, expired} //default?
+    State public currentState;
+    struct LenderAccount {
+        uint amountLent;
+        uint amountRepaid;
+    }
+    
+    //Events
+    event LentToLoan(address addr, uint amount);
+    event DisbursedToBorrower(address addr, uint amount);
+    event RepaidByBorrower(address addr, uint amount);
+    event RepaidToLender(address addr, uint amount);
+    event SentBack(address addr, uint amount);
+    event LoanFunded();
+    event LoanExpired();
+    event LoanRepaid();
+    event LoanBecameDelinquent();
+    
+    // Constructor
+    function LoanContract5(
+        address _borrowerAddress, 
+        uint loanAmountInEthers,
+        uint fundRaisingDurationInDays,
+        uint repaymentDurationInDays
+    ) {
+        borrowerAddress = _borrowerAddress;
+        loanAmount = loanAmountInEthers * 1 ether;
+        fundRaisingDeadline = now + fundRaisingDurationInDays * 1 days;
+        repaymentDeadline = now + repaymentDurationInDays * 1 days;
+    }
+
+    // To keep things simple, the default function handles all logic for both lenders and borrowers
+    function() payable {
+        if (currentState == State.raising) {
+            lend();
+        } else if (currentState == State.repaying) {
+            repay();
+        } else {
+            throw;
+        }
+    }
+    
+    // Lender sends wei to the contract, we store how much they lent
+    function lend() private {
+        // Don't allow borrowers to lend
+        if (msg.sender == borrowerAddress) {
+            throw;
+        }
+        // Don't allow overfunding
+        if (msg.value > amountLeftToFund()) {
+            throw;
+        }
+        // Ensure that lenders amount is divisible by the share price. This makes
+        // the repayment logic simplier, and avoids situations like 1 wei lends
+        uint amountLent = (msg.value / sharePrice) * sharePrice;
+        
+        // Handle lender lending twice
+        if (lenderAccounts[msg.sender].amountLent == 0) {
+            numLenders++;
+            lenderAddresses.push(msg.sender);
+            lenderAccounts[msg.sender] = LenderAccount(amountLent, 0);
+        } else {
+            lenderAccounts[msg.sender].amountLent += amountLent;
+        }
+        
+        amountRaised += amountLent;
+        LentToLoan(msg.sender, amountLent);
+        
+        checkLoanFunded();
+        
+        returnToSender(msg.value - amountLent);
+    }
+    
+    // Send back any extra: note this currently runs out of gas and I can't figure out why
+    function returnToSender(uint amount) private {
+        if (amount > 0) {
+            if (msg.sender.send(amount)) {
+                SentBack(msg.sender, amount);
+            }
+        }
+    }
+    
+    // Disburse to borrowers account, and move to repaying
+    // Note the last lender pays the gas on this but it isn't very expensive)
+    function checkLoanFunded() private {
+        if (amountRaised >= loanAmount) {
+            LoanFunded();
+            if (borrowerAddress.send(amountRaised)) {
+                currentState = State.repaying;
+                DisbursedToBorrower(borrowerAddress, amountRaised);
+            } //@todo error case?
+        }
+    }
+    
+    // Borrower sends wei to the contract, we disburse to all the lenders
+    function repay() private {
+        // Only borrowers can repay, and can't repay more than the amount left
+        if (msg.sender != borrowerAddress) {
+            throw;
+        }
+        if (msg.value > amountLeftToRepay()) {
+            throw;
+        }
+        
+        // Distribute wei evenly to lenders, if there's a remainder send it back to the borrower
+        uint amountDistributed = 0;
+        for (uint i = 0; i < lenderAddresses.length; i++) {
+            address currentLender = lenderAddresses[i];
+            uint amountForLender = (msg.value * lenderAccounts[currentLender].amountLent) / loanAmount; /* Division in solidity throws away the remainder*/
+            if (amountForLender > 0) {
+                if (currentLender.send(amountForLender)) {
+                    RepaidToLender(currentLender, amountForLender);
+                    lenderAccounts[currentLender].amountRepaid += amountForLender;
+                    amountDistributed += amountForLender;
+                } //@todo error case?
+            }
+        }
+        
+        amountRepaid += amountDistributed;
+        RepaidByBorrower(borrowerAddress, amountDistributed);
+        
+        checkLoanRepaid();
+        
+        returnToSender(msg.value - amountDistributed);
+    }
+    
+    function checkLoanRepaid() private {
+        if (amountRepaid == loanAmount) {
+            LoanRepaid();
+            currentState = State.repaid;
+        }
+    }
+    
+    /* Useful constant functions */
+    
+    function amountLeftToFund() constant returns (uint) {
+        return loanAmount - amountRaised;
+    }
+    
+    function amountLeftToRepay() constant returns (uint) {
+        return loanAmount - amountRepaid;
+    }
+    
+    function isDelinquent() constant returns (bool) {
+        return (now >= repaymentDeadline && currentState != State.repaid);
+    }
+    
+    // Anyone can call this, but it will probably be an admin
+    // If the expiration date has passed, send wei back to lenders
+    function makeExpired() {
+         if (now >= fundRaisingDeadline && currentState != State.expired) {
+            currentState = State.expired;
+            LoanExpired();
+            for (uint i = 0; i < lenderAddresses.length; i++) {
+                address currentLender = lenderAddresses[i];
+                if (currentLender.send(lenderAccounts[currentLender].amountLent)) {
+                    RepaidToLender(currentLender, lenderAccounts[currentLender].amountLent);
+                } //@todo error case?
+            }
+        }
+    }
+}
